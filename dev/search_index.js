6a916d635dec94a00e3fccee8402bfc9790885f1
Documenter.jl <documenter@juliadocs.github.io>
2021-03-09 01:16:25 +0000

    build based on fe0cbfe

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = AtomicMiscellany","category":"page"},{"location":"#AtomicMiscellany","page":"Home","title":"AtomicMiscellany","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [AtomicMiscellany]","category":"page"}]
+}
