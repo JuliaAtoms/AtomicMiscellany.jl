7541dcb224464acfbecb0bfdfb5f0a952474087b
Morten Piibeleht <morten.piibeleht@gmail.com>
2021-06-03 16:10:05 +1200

    Set GKSwstype in make.jl instead
    It is also useful when building the Documenter pages locally.

--- docs/make.jl
+++ docs/make.jl
@@ -2,6 +2,12 @@ using AtomicMiscellany
 using Documenter, Literate
 using Plots, Formatting
 
+# Stop GR from trying to connect to X when producing plots.
+# This will make the documentation build about x10 faster and gets rid of all the
+# GR error messages from the build logs.
+# https://discourse.julialang.org/t/generation-of-documentation-fails-qt-qpa-xcb-could-not-connect-to-display/60988/2
+ENV["GKSwstype"] = "100"
+
 DocMeta.setdocmeta!(AtomicMiscellany, :DocTestSetup, quote
     using AtomicMiscellany, AtomicMiscellany.NuclearModels
 end; recursive=true)
